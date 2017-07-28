a73927c0e340a497f5d16b3408679550197e5361
su2 <2419248570@qq.com>
2017-07-28 19:10:09 +0800

    创建了ViewUtils工程

---
+++ src/com/it/hei/MainActivity.java
@@ -0,0 +1,45 @@
+package com.it.hei;
+
+import com.example.myviewutils.OnClick;
+import com.example.myviewutils.ViewInject;
+import com.example.myviewutils.ViewUtils;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+	@ViewInject(R.id.tv1)
+	private TextView tv1;
+	
+	@ViewInject(R.id.tv2)
+	private TextView tv2;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		ViewUtils.inject(this);
+	}
+
+	@OnClick({R.id.btn1,R.id.btn2})
+	public void k(View v){
+		switch (v.getId()) {
+		case R.id.btn1:
+			Toast.makeText(this, "btn1:"+tv1.getText(), Toast.LENGTH_SHORT).show();
+			break;
+
+		case R.id.btn2:
+			Toast.makeText(this, "btn2:"+tv2.getText(), Toast.LENGTH_SHORT).show();
+			break;
+		default:
+			break;
+		}
+	}
+	
+}
